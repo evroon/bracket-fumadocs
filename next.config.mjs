60fdf67b57a0090ce50bee0c914df0832c29da61
Erik Vroon <erik.vroon@channable.com>
2025-02-26 08:05:24 +0100

    initial

---
+++ next.config.mjs
@@ -0,0 +1,14 @@
+import nextra from 'nextra'
+
+const withNextra = nextra({
+  contentDirBasePath: '/' // Or even nested e.g. `/docs/advanced`
+})
+
+// You can include other Next.js configuration options here, in addition to Nextra settings:
+export default withNextra({
+  // Export only when building in GitHub Actions
+  output: process.env.GITHUB_ACTION ? 'export' : undefined,
+  images: {
+    unoptimized: true
+  }
+})
