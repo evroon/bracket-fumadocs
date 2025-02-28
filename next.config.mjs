0645d04f25e606b89f134883298f3ecc1e237278
Erik Vroon <erik.vroon@channable.com>
2025-02-28 17:25:56 +0100

    fixup! fixup! fixup! initial

--- next.config.mjs
+++ next.config.mjs
@@ -7,7 +7,6 @@ const withNextra = nextra({
 // You can include other Next.js configuration options here, in addition to Nextra settings:
 export default withNextra({
   reactStrictMode: true,
-  basePath: "/bracket-fumadocs",
   // Export only when building in GitHub Actions
   output: process.env.GITHUB_ACTION ? 'export' : undefined,
   images: {
